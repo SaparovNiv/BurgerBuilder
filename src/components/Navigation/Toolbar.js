77d9eab38f7a72327b4ebb5c359d8db87af0e59e
nivsaparov <nivsaparov1@gmail.com>
2021-03-31 14:30:32 +0300

    add toolbar style

--- src/components/Navigation/Toolbar.js
+++ src/components/Navigation/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import classes from './Toolbar.module.css'
 const toolbar = (props) => (
-    <header>
+    <header className={class.Toolbar}>
         <div>MENU</div>
         <div>LOGO</div>
         <nav>
